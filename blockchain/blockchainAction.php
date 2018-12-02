d98a4e84e755281718db378ae3d57c93f241a324
PaulHawkings <paulhawking_pan@hotmail.com>
2018-12-03 00:03:54 +0800

    Implement server side GET logic - Get account balance

--- blockchain/blockchainAction.php
+++ blockchain/blockchainAction.php
@@ -2,31 +2,50 @@
 
 function printLog($message)
 {
-    $logFile = fopen("blockchainData.log", 'w');
-    fwrite($logFile, $message);
+    $logFile = fopen("blockchainData.log", 'a');
+    fwrite($logFile, $message . "\n");
     fclose($logFile);
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET')
 {
-    $string = file_get_contents("blockchainData.json");
-    $blockchainJson = json_decode($string, true);
-    $blockCount = count($blockchainJson["blockchain"]);
-    echo $blockchainJson["blockchain"][0]["transaction"]["value"];
+    printLog("IN");
 
-    $fp = fopen("blockchainData.json", 'w');
-    fwrite($fp, json_encode($blockchainJson, JSON_PRETTY_PRINT));
-    fclose($fp);
+    if (isset($_GET['usr']))
+    {
+        // Get user balance
+        $username = $_GET['usr'];
+        $balance = 0;
+
+        $string = file_get_contents("blockchainData.json");
+        $blockchainJson = json_decode($string, true);
+
+        foreach ($blockchainJson["blockchain"] as $block)
+        {
+            if ($block["transaction"]["from"] == $username)
+            {
+                printLog("from " . $username . " " . $block["transaction"]["value"]);
+                $balance -= $block["transaction"]["value"];
+            }
+            else if ($block["transaction"]["to"] == $username)
+            {
+                printLog("to " . $username . " " . $block["transaction"]["value"]);
+                $balance += $block["transaction"]["value"];
+            }
+        }
+    }
+
+    echo $balance;
 }
 elseif ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
-    $post = file_get_contents('php://input');
+    $data = file_get_contents('php://input');
 
     $string = file_get_contents("blockchainData.json");
     $blockchainJson = json_decode($string, true);
     $blockCount = count($blockchainJson["blockchain"]);
 
-    $blockchainJson["blockchain"][$blockCount] = json_decode($post, JSON_PRETTY_PRINT);
+    $blockchainJson["blockchain"][$blockCount] = json_decode($data, JSON_PRETTY_PRINT);
 
     $fp = fopen("blockchainData.json", 'w');
     fwrite($fp, json_encode($blockchainJson, JSON_PRETTY_PRINT));
